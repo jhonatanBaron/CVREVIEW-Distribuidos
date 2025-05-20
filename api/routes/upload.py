7a34bf694397e0e536e5b7630da6606d4a629120
Jhonatan Baron <jhonatan.baron@uptc.edu.co>
2025-05-20 17:21:24 -0500

    cambios reflejados del upload

--- api/routes/upload.py
+++ api/routes/upload.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from uuid import uuid4
 from api.shared.queue_config import get_channel
-
-
 from services.parser import save_uploaded_file
 from database.database import save_cv_request
-import os
+import json
+import pika 
 
+#el objetivo es recibir un CV y guardarlo en la base de datos
 router = APIRouter()
 
 @router.post("/upload")
@@ -22,6 +22,27 @@ async def upload_cv(
     cv_id = str(uuid4())
     file_path = await save_uploaded_file(file, cv_id)
 
+    # Guardar en DB
     save_cv_request(cv_id=cv_id, nombre=nombre, email=email, puesto=puesto, filename=file_path)
+
+    # Enviar mensaje a RabbitMQ
+    #encolar el mensaje en la cola parser_queue
+    # el mensaje contiene el id del cv, el nombre del archivo y el email
+    try:
+        channel, conn = get_channel()
+        payload = {
+            "cv_id": cv_id,
+            "filename": file_path,
+            "email": email
+        }
+        channel.basic_publish(
+            exchange='',
+            routing_key='parser_queue',
+            body=json.dumps(payload),
+            properties=pika.BasicProperties(delivery_mode=2)  # persistente
+        )
+        conn.close()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error encolando mensaje: {str(e)}")
 
     return {"message": "CV recibido", "cv_id": cv_id}
